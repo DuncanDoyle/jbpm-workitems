32e56dd90cfef5e768d5a8788b6693143fec2452
Duncan Doyle <Duncan.Doyle@gmail.com>
2015-05-18 00:08:20 +0200

    Rebuild the entire CommandClass due to JBPM-4655. When that JIRA is resolved, we can revert this commit.

--- cxf-webservice-workitem/src/main/java/org/jboss/ddoyle/jbpm/process/workitem/webservice/cxf/CxfWebServiceCommand.java
+++ cxf-webservice-workitem/src/main/java/org/jboss/ddoyle/jbpm/process/workitem/webservice/cxf/CxfWebServiceCommand.java
@@ -16,9 +16,23 @@
 
 package org.jboss.ddoyle.jbpm.process.workitem.webservice.cxf;
 
+import java.lang.reflect.Array;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.xml.namespace.QName;
+
 import org.apache.cxf.Bus;
+import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.dynamic.DynamicClientFactory;
+import org.apache.cxf.message.Message;
 import org.jbpm.process.workitem.webservice.WebServiceCommand;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.internal.executor.api.Command;
+import org.kie.internal.executor.api.CommandContext;
+import org.kie.internal.executor.api.ExecutionResults;
+import org.kie.internal.runtime.Cacheable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Extension of the <code>jBPM 6</code> default {@link WebServiceWorkItemCommand}. This implementation allows one to provide a
@@ -29,28 +43,108 @@ import org.jbpm.process.workitem.webservice.WebServiceCommand;
  * 
  * @author <a href="mailto:duncan.doyle@redhat.com">Duncan Doyle</a>
  */
-public class CxfWebServiceCommand extends WebServiceCommand {
+public class CxfWebServiceCommand implements Command, Cacheable {
 
 	private static final String CXF_CONFIGURATION_FILE_URL = "cxf.xml";
+	
+	private static final Logger logger = LoggerFactory.getLogger(WebServiceCommand.class);
+    private volatile static ConcurrentHashMap<String, Client> clients = new ConcurrentHashMap<String, Client>();
+    private DynamicClientFactory dcf = null;
 
-	private DynamicClientFactory dcf = null;
+    @Override
+    public ExecutionResults execute(CommandContext ctx) throws Exception {
+    	// since JaxWsDynamicClientFactory will change the TCCL we need to restore it after creating client
+        ClassLoader origClassloader = Thread.currentThread().getContextClassLoader();
+        try {
+	    	Object[] parameters = null;
+	        WorkItem workItem = (WorkItem) ctx.getData("workItem");
+	        
+	        String interfaceRef = (String) workItem.getParameter("Interface");
+	        String operationRef = (String) workItem.getParameter("Operation");
+	        String endpointAddress = (String) workItem.getParameter("Endpoint");
+	        if ( workItem.getParameter("Parameter") instanceof Object[]) {
+	        	parameters =  (Object[]) workItem.getParameter("Parameter");
+	        } else if (workItem.getParameter("Parameter") != null && workItem.getParameter("Parameter").getClass().isArray()) {
+	        	int length = Array.getLength(workItem.getParameter("Parameter"));
+	            parameters = new Object[length];
+	            for(int i = 0; i < length; i++) {
+	            	parameters[i] = Array.get(workItem.getParameter("Parameter"), i);
+	            }            
+	        } else {
+	        	parameters = new Object[]{ workItem.getParameter("Parameter")};
+	        }
+	        
+	        Client client = getWSClient(workItem, interfaceRef, ctx);
+	        
+	        //Override endpoint address if configured.
+	        if (endpointAddress != null && !"".equals(endpointAddress)) {
+	       	 client.getRequestContext().put(Message.ENDPOINT_ADDRESS, endpointAddress) ;
+	        }
+	        
+	        Object[] result = client.invoke(operationRef, parameters);
+	        
+	        ExecutionResults results = new ExecutionResults();       
+	
+	        if (result == null || result.length == 0) {
+	            results.setData("Result", null);
+	        } else {
+	            results.setData("Result", result[0]);
+	        }
+	        logger.debug("Received sync response {}", result);
+	        
+	        
+	        return results;
+        }finally {
+    		Thread.currentThread().setContextClassLoader(origClassloader);
+    	}
+    }
+    
+    
+    protected synchronized Client getWSClient(WorkItem workItem, String interfaceRef, CommandContext ctx) {
+        if (clients.containsKey(interfaceRef)) {
+            return clients.get(interfaceRef);
+        }
+        
+        String importLocation = (String) workItem.getParameter("Url");
+        String importNamespace = (String) workItem.getParameter("Namespace");
+        if (importLocation != null && importLocation.trim().length() > 0 
+                && importNamespace != null && importNamespace.trim().length() > 0) {
+        	
+            Client client = getDynamicClientFactory(ctx).createClient(importLocation, new QName(importNamespace, interfaceRef), Thread.currentThread().getContextClassLoader(), null);
+            clients.put(interfaceRef, client);
+            return client;
+        	
+        }
 
-	/**
+        return null;
+    }
+    
+    /**
 	 * Creates a new {@link DynamicClientFactory}. Uses the {@link CxfDynamicClientFactoryBuilder} to build a new
 	 * {@link DynamicClientFactory} from the <code>cxf.xml</code> config file found on the classpath.
 	 * 
 	 * @return the {@link DynamicClientFactory}
 	 */
-	@Override
-	protected synchronized DynamicClientFactory getDynamicClientFactory() {
+	protected synchronized DynamicClientFactory getDynamicClientFactory(CommandContext ctx) {
 		if (this.dcf == null) {
-			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			//Retrieve the classloader from the context.
+			ClassLoader cl = (ClassLoader) ctx.getData("ClassLoader");
 			if (cl == null) {
-				cl = this.getClass().getClassLoader();
+				cl = Thread.currentThread().getContextClassLoader();
 			}
 			this.dcf = new CxfDynamicClientFactoryBuilder().setClassLoader(cl).setCxfConfigurationFile(CXF_CONFIGURATION_FILE_URL).build();
 		}
 		return this.dcf;
 	}
+    
+	@Override
+	public void close() {
+		if (clients != null) {
+			for (Client client : clients.values()) {
+				client.destroy();
+			}
+		}
+	}
 
+	
 }
